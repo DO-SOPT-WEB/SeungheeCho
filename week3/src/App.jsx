3e0cb74f50a3bdcdb0d0a0b37526f5b3e8aba349
lydiacho <sheecho0804@ewhain.net>
2023-11-02 00:47:20 +0900

    style: 전체 레이아웃 잡기

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -1,11 +1,26 @@
 import Header from "./components/Header";
+import Onboarding from "./components/Onboarding";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
-      <Header />
+      <Wrapper>
+        <Header />
+        <Onboarding />
+      </Wrapper>
     </>
   );
 }
 
 export default App;
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+
+  width: 100vw;
+  height: 100vh;
+`;
---
+++ week3/src/components/Onboarding.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Onboarding = () => {
+  return <OnboardingWrapper></OnboardingWrapper>;
+};
+
+export default Onboarding;
+
+const OnboardingWrapper = styled.main`
+  width: 60%;
+  height: 60%;
+
+  background-color: pink;
+`;
