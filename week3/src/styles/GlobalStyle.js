4b191a1af38ed4f8a742b7197989603ed0ca8b24
lydiacho <sheecho0804@ewhain.net>
2023-11-01 22:16:59 +0900

    setting: 스타일 라이브러리 설치 및 전역스타일 세팅

---
+++ week3/src/styles/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+${reset}
+@font-face {
+    font-family: 'KOTRAHOPE';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/KOTRAHOPE.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+#root, body, html {
+    width:100%;
+    height: 100vh;
+    margin: 0 auto;
+}
+  
+* {
+    font-family: 'KOTRAHOPE';
+    box-sizing: border-box;
+}
+    
+`;
+
+export default GlobalStyle;
