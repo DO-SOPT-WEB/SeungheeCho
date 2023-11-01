dd90966ad7a6db125a259f626cea2a870445f5d1
lydiacho <sheecho0804@ewhain.net>
2023-11-02 01:55:18 +0900

    feat: Onboarding hover, 추천방식 선택시 UI

--- week3/src/components/Onboarding.jsx
+++ week3/src/components/Onboarding.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Onboarding = () => {
+  const [howPick, setHowPick] = useState("");
+
   return (
     <OnboardingWrapper>
       <OnboardingHeader>
         <h2>추천 방식을 골라주세요</h2>
       </OnboardingHeader>
-      <OnboardingBox>
-        <OnboardingCard>취향대로 추천</OnboardingCard>
-        <OnboardingCard>랜덤 추천</OnboardingCard>
+      <OnboardingBox $howPick={howPick}>
+        <OnboardingCard
+          onClick={() => {
+            setHowPick("취향대로 추천");
+          }}>
+          취향대로 추천
+        </OnboardingCard>
+        <OnboardingCard
+          onClick={() => {
+            setHowPick("랜덤 추천");
+          }}>
+          랜덤 추천
+        </OnboardingCard>
       </OnboardingBox>
+      <OnboardingPick $howPick={howPick}>{howPick}</OnboardingPick>
+
+      <OnboardingStartBtn $howPick={howPick}> Start!</OnboardingStartBtn>
     </OnboardingWrapper>
   );
 };
@@ -35,7 +51,7 @@ const OnboardingHeader = styled.header`
 `;
 
 const OnboardingBox = styled.section`
-  display: flex;
+  display: ${(props) => (props.$howPick === "" ? "flex" : "none")};
   gap: 3rem;
 
   margin-top: 4rem;
@@ -53,6 +69,42 @@ const OnboardingCard = styled.button`
   background-color: white;
 
   font-size: 2rem;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ffe1e6;
+  }
+`;
+
+const OnboardingPick = styled.button`
+  display: ${(props) => (props.$howPick === "" ? "none" : "flex")};
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 4rem;
+  width: 43vw;
+  height: 30vh;
+
+  border-radius: 1rem;
+  background-color: white;
+
+  font-size: 2rem;
+`;
+
+const OnboardingStartBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  visibility: ${(props) => (props.$howPick === "" ? "hidden" : "visible")};
+
+  margin-top: 2rem;
+  width: 5rem;
+  height: 2rem;
+
+  border-radius: 1rem;
+  background-color: black;
+  color: white;
 
   cursor: pointer;
 `;
