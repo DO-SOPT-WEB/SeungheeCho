bb551ec1b53b3e39500b6e056ca85ec5c4a92bc6
lydiacho <sheecho0804@ewhain.net>
2023-11-17 06:27:29 +0900

    feat: Home 컴포넌트 구현

--- week4/src/pages/Home.jsx
+++ week4/src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import Login from "./Login";
+import { useNavigate } from "react-router-dom";
+import Buttons from "../components/Buttons";
+import Layout from "../components/Layout";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const btnInfo = [
+    {
+      name: "로그인하러 가기",
+      clickAction: function () {
+        navigate("/login");
+      },
+    },
+  ];
+
   return (
     <div>
-      <Login />
+      <Layout title="33th SOPT" buttons={Buttons(btnInfo)} />
     </div>
   );
 };
