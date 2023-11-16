8bf8b8c7191f0185104d5907cc049fd110a0b065
lydiacho <sheecho0804@ewhain.net>
2023-11-17 06:53:46 +0900

    feat: 로그인 에러메시지 모달 구현

--- week4/src/components/Layout.jsx
+++ week4/src/components/Layout.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 // 모달 컴포넌트 (공통)
 const Layout = ({ title, buttons, children }) => {
   return (
-    <Wrapper>
+    <Wrapper className="top">
       <Container>
         <Header>
           <h1>{title}</h1>
@@ -19,8 +19,11 @@ export default Layout;
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  position: relative;
 
   width: 100vw;
   height: 100vh;
--- week4/src/pages/Login.jsx
+++ week4/src/pages/Login.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router";
 import postLogin from "../api/postLogin";
 import Buttons from "../components/Buttons";
 import InputContainer from "../components/InputContainer";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
 
 // 로그인 컴포넌트 (페이지)
 const Login = () => {
   const [ID, setId] = useState("");
   const [PW, setPw] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
 
   const navigate = useNavigate();
 
@@ -16,7 +20,7 @@ const Login = () => {
     {
       name: "로그인",
       clickAction: function () {
-        postLogin(ID, PW, navigate);
+        postLogin(ID, PW, navigate, setModalOpen, setErrMsg);
       },
     },
     {
@@ -31,8 +35,29 @@ const Login = () => {
     <Layout title="Login" buttons={Buttons(btnInfo)}>
       <InputContainer name="ID" state={ID} setState={setId} />
       <InputContainer name="PASSWORD" state={PW} setState={setPw} />
+      {modalOpen &&
+        createPortal(
+          <Modal>{errMsg}</Modal>,
+          document.querySelectorAll(".top")[0]
+        )}
     </Layout>
   );
 };
 
 export default Login;
+
+const Modal = styled.section`
+  text-align: center;
+
+  position: absolute;
+  bottom: 7rem;
+  padding: 1rem;
+
+  font-size: 1.3rem;
+  font-weight: 600;
+
+  border-radius: 0.7rem;
+  border: 0;
+  box-shadow: 0 0 0.3rem red;
+  background-color: #ffd0d0;
+`;
