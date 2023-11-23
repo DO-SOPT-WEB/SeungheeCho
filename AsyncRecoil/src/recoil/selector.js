0bacf7797d7f0aac98dd5c15337d5699a8087f1d
lydiacho <sheecho0804@ewhain.net>
2023-11-23 18:39:19 +0900

    setting: react-error-boundary 설치

--- AsyncRecoil/src/recoil/selector.js
+++ AsyncRecoil/src/recoil/selector.js
@@ -6,7 +6,7 @@ export const getUserInfoQuery = selector({
   get: async ({ get }) => {
     console.log("selector get 실행");
     try {
-      const res = await axios.get("http://3.39.54.196/api/v1/members/300");
+      const res = await axios.get("http://3.39.54.196/api/v1/members/900");
       return res.data;
     } catch (err) {
       console.error(err);
